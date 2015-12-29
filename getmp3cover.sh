b51c6318bdcd0ee5348ebcf34b14a0c8b82572de
nelsonchung <chihchun.chung@gmail.com>
2015-12-29 13:22:40 +0800

    Add getmp3cover.sh. Main application.

---
+++ getmp3cover.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+SONG_FILE="song.html"
+ALBUM_FILE="album.html"
+MP3_FILE="mp3cover.html"
+ALBUM_KEYWORD="<\/strong> - Album Version"
+
+#0. Clear data
+rm $SONG_FILE 
+rm $ALBUM_FILE
+rm $MP3_FILE 
+
+#1. Get the information of song (三年二班)
+echo "1. Get the information of song"
+#curl -o song.html https://www.kkbox.com/tw/tc/search.php?word=三年二班&search=mix&lang=tc
+curl -o $SONG_FILE https://www.kkbox.com/tw/tc/search.php?word=$1&search=mix&lang=tc
+#We need to wait 3~5 seconds if we use curl utility
+sleep 5
+
+#2. Get the information of album (葉惠美) included the song
+echo "2. Get the information of album"
+#cat song.html | grep "三年二班<\/strong> - Album Version" -C10 | tail -n 1 | awk -F" " '{print $1}'
+ALBUM_NAME=`cat $SONG_FILE | grep "$1$ALBUM_KEYWORD" -C10 | tail -n 1 | awk -F" " '{print $1}'`
+
+#3. Get the cover of album
+#curl -o album.html https://www.kkbox.com/tw/tc/search.php?search=mix&word=葉惠美&lang=tc
+#curl -o $ALBUM_FILE https://www.kkbox.com/tw/tc/search.php?search=mix&word=$ALBUM_NAME&lang=tc
+python getmp3cover.py $ALBUM_NAME 
+
+#4. Parse the mp3 cover link
+cat $MP3_FILE | grep $ALBUM_NAME | grep jpg
