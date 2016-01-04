7560ed4734ab4717e5fabdb24b842f499127cc7f
nelsonchung <chihchun.chung@gmail.com>
2016-01-04 13:26:03 +0800

    Move global settings to global_setting.sh

--- getmp3cover.sh
+++ getmp3cover.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-SONG_FILE="song.html"
-ALBUM_FILE="album.html"
-MP3_FILE="mp3cover.html"
-MP3_COVER_FILE="mp3_cover.jpg"
+source global_setting.sh
 #ALBUM_KEYWORD="<\/strong> - Album Version"
 ALBUM_KEYWORD="<strong class=\"keyword c1\">"
 SONG_INDEX_KEYWORD_START="alt=\""
