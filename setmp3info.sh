1f6b32d7736141aaa24a68f96e97dc4b5abd80cc
nelsonchung <chihchun.chung@gmail.com>
2016-01-04 13:27:48 +0800

    Add new file - setmp3info.sh to parse the mp3 files without mp3 tag information and just have song name.

---
+++ setmp3info.sh
@@ -0,0 +1,36 @@
+source global_setting.sh
+SONG_NAME="$1"
+echo "Try to get $SONG_NAME mp3 file................................................................................................................"
+#0. Clear data
+rm $SONG_FILE 
+rm $ALBUM_FILE
+rm $MP3_FILE 
+
+#1. Get the information of song (三年二班)
+echo "1. Get the information of song"
+curl -o $SONG_FILE https://www.kkbox.com/tw/tc/search.php?word=$SONG_NAME&search=song
+#We need to wait 3~5 seconds if we use curl utility
+sleep 5 
+
+#2. Get the information of album (葉惠美) included the song
+echo "2. Get the information of artist"
+#ARTIST_NAME=`cat $SONG_FILE | grep "$ALBUM_KEYWORD$SONG_NAME" -C4 | tail -n 1 | awk -F" " '{print $SONG_NAME}'`
+ARTIST_NAME=`cat $SONG_FILE | grep "$ALBUM_KEYWORD$SONG_NAME" -C4 | tail -n 1 | awk -F"html\">" '{print $2}' | awk -F"</a>" '{print $1}'`
+echo $ARTIST_NAME
+if [ "$ARTIST_NAME" == "" ]; then #some song need to shift to get correct artist name
+    echo "Not get artist name, so we need to to C11 setting"
+    ARTIST_NAME=`cat $SONG_FILE | grep "$ALBUM_KEYWORD$SONG_NAME" -C5 | tail -n 1 | awk -F"html\">" '{print $2}' | awk -F"</a>" '{print $1}'`
+    echo $ARTIST_NAME
+fi
+ 
+echo "3. Get the informatin of album"
+ALBUM_NAME=`cat $SONG_FILE | grep "$ALBUM_KEYWORD$SONG_NAME" -C10 | tail -n 1 | awk -F" " '{print $1}'`
+echo $ALBUM_NAME
+if [ "$ALBUM_NAME" == "" ]; then #some song need to shift to get correct album name
+    echo "Not get album name, so we need to to C11 setting"
+    ALBUM_NAME=`cat $SONG_FILE | grep "$ALBUM_KEYWORD$SONG_NAME" -C11 | tail -n 1 | awk -F" " '{print $1}'`
+    echo $ALBUM_NAME
+fi
+
+id3v2 -D $1".mp3"
+id3v2 -t $SONG_NAME -a $ARTIST_NAME -A $ALBUM_NAME NAME $1".mp3"
